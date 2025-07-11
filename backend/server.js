97bc565b61c59488f174721059789f04e8d09da2
Sakyiama Dennis <albacruise99@gmail.com>
2025-07-11 10:16:10 +0000

    changes origin url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://htmltopdf-b7ag.onrender.com",
     methods: ["POST"],
     allowedHeaders: ["Content-Type"],
   })
